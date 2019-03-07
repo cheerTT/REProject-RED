94a8b40f989ca82c8594cb39806b824d1f1c7ca6
CatherineXX <1262356621@qq.com>
2019-03-07 10:56:20 +0800

    Merge branch 'master' of D:\Documents\djangoDev\REProject-RED with conflicts.

--- reporjectred/settings.py
+++ reporjectred/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'NAME': 'reprojectred',
         'HOST': '127.0.0.1',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '123456',
         'PORT': '3306'
     }
 }
