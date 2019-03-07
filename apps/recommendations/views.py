f99960a0e36a873559e6c4d2bd11ef98f6d93323
Undercut <646866816@qq.com>
2019-03-07 09:44:40 +0800

    更新一次

--- apps/recommendations/views.py
+++ apps/recommendations/views.py
@@ -15,7 +15,6 @@ from django.db.models import Q
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
-# Create your views here.
 
 class RecommendationsView(LoginRequiredMixin, View):
 
