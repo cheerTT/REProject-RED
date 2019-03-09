0b8b79376c5f5ceae5f9d9d5bff291ed6638eabc
xiaoxiaokun <qweas329@vip.qq.com>
2019-03-09 22:42:32 +0800

    基本实现 增加，删减热门商品功能

--- apps/hotcommend/models.py
+++ apps/hotcommend/models.py
@@ -11,14 +11,14 @@ from django.db import models
 #         db_table = 'commendation'
 
 
-class Sales_Counts(models.Model):
-    assin = models.CharField(max_length=100, blank=False, null=False)
+class hot_list(models.Model):
+    assin = models.CharField(max_length=200, blank=False, null=False)
     title = models.CharField(max_length=200, blank=True, null=True)
-    type = models.CharField(max_length=20, blank=True, null=True)
+    categories = models.CharField(max_length=20, blank=True, null=True)
     sales_count = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
-        db_table = 'sale_counts'
+        db_table = 'hot_list'
 
 class transaction_record(models.Model):
     user_id = models.CharField(max_length=255, blank=False, null=False)
