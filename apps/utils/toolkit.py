234da09de06be6d57e7a64bd73ab4cb099fc6f51
ttwen <1014642494@qq.com>
2019-03-10 10:34:44 +0800

    生成用户性别统计

--- apps/utils/toolkit.py
+++ apps/utils/toolkit.py
@@ -37,29 +37,55 @@ def get_month_member_count(value=0):
     month_member_count.append(data)
     return month_member_count
 
+def  get_member_gender(value=0):
+    '''
+    生成用户性别数据
+    '''
+    filters = dict()
+    member_gender = []
+
+    count = []
+    # start_date = date.today().replace(month=month, day=1)
+    # _, days_in_month = calendar.monthrange(start_date.year, start_date.month)
+    # end_date = start_date + timedelta(days_in_month - 1)
+    filters['gender'] = '1'
+    member_gender1 = Member.objects.filter(**filters).count()
+    print("member_gender1:", member_gender1)
+    count.append(member_gender1)
+
+    filters['gender'] = '2'
+    member_gender2 = Member.objects.filter(**filters).count()
+    print("member_gender2:",member_gender2)
+    count.append(member_gender2)
+    data = {
+        'count': count
+    }
+    print('data:',data)
+    member_gender.append(data)
+    return member_gender
 
 def get_year_work_order_count(users, value=0):
     """
     生成年度统计数据
     """
-    filters = dict()
-    year_work_order_count = []
-    for user in users:
-        start_year = date.today().replace(month=1, day=1)
-        end_year = date.today().replace(year=(start_year.year + 1), month=1, day=1)
-        filters['add_time__range'] = (start_year, end_year)
-        if value == 0:
-            filters['proposer_id'] = user['id']
-        else:
-            filters['receiver_id'] = user['id']
-        year_work_order = WorkOrder.objects.filter(**filters).count()
-        data = {
-            'name': user['name'],
-            'count': year_work_order
-        }
-        year_work_order_count.append(data)
-
-    return year_work_order_count
+    # filters = dict()
+    # year_work_order_count = []
+    # for user in users:
+    #     start_year = date.today().replace(month=1, day=1)
+    #     end_year = date.today().replace(year=(start_year.year + 1), month=1, day=1)
+    #     filters['add_time__range'] = (start_year, end_year)
+    #     if value == 0:
+    #         filters['proposer_id'] = user['id']
+    #     else:
+    #         filters['receiver_id'] = user['id']
+    #     year_work_order = WorkOrder.objects.filter(**filters).count()
+    #     data = {
+    #         'name': user['name'],
+    #         'count': year_work_order
+    #     }
+    #     year_work_order_count.append(data)
+    #
+    # return year_work_order_count
 
 
 class ToolKit(object):
