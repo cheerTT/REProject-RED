c5767caa3d80c916c809c5a7d481936e16548248
CatherineXX <1262356621@qq.com>
2019-03-10 10:25:18 +0800

    添加商品图片

---
+++ apps/member/apps.py
@@ -0,0 +1,11 @@
+'''
+@Author:ttwen
+@Time:2019年3月7日10:45:35
+@Description: 会员管理的apps配置
+'''
+
+from django.apps import AppConfig
+
+
+class MemberConfig(AppConfig):
+    name = 'member'
---
+++ apps/member/tests.py
@@ -0,0 +1,9 @@
+'''
+@Author:ttwen
+@Time:2019年3月7日10:45:35
+@Description: 会员管理的test方法
+'''
+
+from django.test import TestCase
+
+# Create your tests here.
