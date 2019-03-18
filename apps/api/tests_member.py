61aa771469b97f7dd33db2077823ef79c5082173
cheertt <1913278504@qq.com>
2019-03-18 19:40:23 +0800

    代码恢复

---
+++ apps/api/tests_member.py
@@ -0,0 +1,4 @@
+# @Author : cheertt
+# @Time   : 2019/3/15 15:28
+# @Remark :  与小程序界面交互单元测试
+
--- reporjectred/settings.py
+++ reporjectred/settings.py
@@ -92,28 +92,27 @@ WSGI_APPLICATION = 'reporjectred.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'reprojectred',
-        'HOST': '139.219.2.134',
-        'USER': 'root',
-        'PASSWORD': 'Jincan.4943198',
-
-        'PORT': '3306'
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'reprojectred',
-#         'HOST': '127.0.0.1',
+#         'HOST': '139.219.2.134',
 #         'USER': 'root',
-#         'PASSWORD': 'root',
+#         'PASSWORD': 'Jincan.4943198',
 #         'PORT': '3306'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reprojectred',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': '3306'
+    }
+}
 
 if 'test' in sys.argv:
     DATABASES['default'] = {
@@ -188,10 +187,11 @@ SESSION_SAVE_EVERY_REQUEST = True
 MINSIZE =20  # minimum size of face
 THRESHOLD = [0.6, 0.7, 0.7]  # three steps's threshold
 FACTOR =0.709  # scale factor
-MODELPATH = BASE_DIR + os.sep + 'facenet' + os.sep + '20180402-114759'
+# MODELPATH = BASE_DIR + os.sep + 'facenet' + os.sep + '20180402-114759'
+MODELPATH = BASE_DIR + os.sep + 'facenet' + os.sep + '20180408-102900'
 
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024 # 图片最大为16M
-MAX_DISTINCT = 1.00 #设置最大的相似距离，1.22是facenet基于lfw计算得到的
+MAX_DISTINCT = 0.90 #设置最大的相似距离，1.22是facenet基于lfw计算得到的
 
 APPID = 'wx56291ba66ff53a69'
 SECRET = 'f4ecc5552da2ce4241d7fe083da90592'
