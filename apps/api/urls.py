1f8651e58dce73335d614abf26c80bfe8d395cf5
Undercut <646866816@qq.com>
2019-03-22 15:42:46 +0800

    现在能正常根据画像推荐了

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -11,7 +11,7 @@ from api import views_hotcommend
 urlpatterns = [
     # 会员信息增改删查操作
     url(r'^member/$', views_member.MemberView.as_view(), name="member"),
-    url(r'^member/savepic', views_member.MemberUploadFace.as_view(), name="member-save-face"),
+    # url(r'^member/savepic', views_member.MemberUploadFace.as_view(), name="member-save-face"),
     url(r'^member/login', csrf_exempt(views_member.MemberLoginView.as_view()), name="member-login"),
     url(r'^member/checkreg', csrf_exempt(views_member.MemberCheckRegView.as_view()), name="member-checkreg"),
     url(r'^member/info', views_member.MemberInfoView.as_view(), name="member-info"),
@@ -33,6 +33,7 @@ urlpatterns = [
     url(r'^recommendations/toprecommendations', csrf_exempt(views_recommendations.TopRecommendationsView.as_view()), name="TopRecommendations"),
     url(r'^recommendations/allrecommendations', csrf_exempt(views_recommendations.AllRecommendationsView.as_view()),
         name="AllRecommendations"),
+    url(r'^recommendations/commonrecommendations',csrf_exempt(views_recommendations.Get_Common_Recommenadations.as_view()), name="CommonRecommendations"),
 
     url(r'^hotcommend/hot_commodity', views_hotcommend.HotCommodityView.as_view(), name="hot-commodity"),
     # url(r'^user/list$', views.UserListView.as_view(), name="user-list"),
